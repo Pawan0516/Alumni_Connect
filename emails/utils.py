b413f47ad6e54f65c02c94761f5428ef4202a59a
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2025-09-22 00:17:15 +0530

    setting up emails

--- emails/utils.py
+++ emails/utils.py
@@ -20,3 +20,23 @@ def send_admin_onboarding_otp(to_email, otp_code):
     )
     email.attach_alternative(html_content, "text/html")
     email.send()
+
+def send_invite_mail(to_email, otp_code):
+    context = {
+        "member_name": "Alice Smith",
+        "college_name": "ABC College",
+        "invitation_link": "https://alumniconnect.com/join/abc123",
+        "current_year": datetime.now().year
+    }
+
+    subject="You're Invited to Join Alumni Connect"
+    
+
+    html_content = render_to_string("emails/invite_member.html", context)
+    text_content = strip_tags(html_content)
+
+    email = EmailMultiAlternatives(
+        subject, text_content, sender, [to_email]
+    )
+    email.attach_alternative(html_content, "text/html")
+    email.send()
